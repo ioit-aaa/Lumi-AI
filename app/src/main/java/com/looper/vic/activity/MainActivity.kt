97ac7c9cd65ff05b5762257ed3985ff3341d1a43
iamlooper <unwoven@duck.com>
2025-06-07 14:42:34 +0500

    chore: update .gitignore, upgrade GitHub Actions dependencies, and refactor MainActivity for app bar behavior

--- app/src/main/java/com/looper/vic/activity/MainActivity.kt
+++ app/src/main/java/com/looper/vic/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.looper.vic.activity
 
 import android.os.Bundle
 import androidx.navigation.NavController
-import com.google.android.material.appbar.AppBarLayout
 import com.looper.android.support.activity.DrawerNavigationActivity
 import com.looper.vic.R
 
@@ -10,27 +9,29 @@ class MainActivity : DrawerNavigationActivity() {
 
     private val destinationChangeListener =
         NavController.OnDestinationChangedListener { _, destination, _ ->
-            val appbar: AppBarLayout = findViewById(com.looper.android.support.R.id.app_bar_layout)
-
             when (destination.id) {
                 R.id.fragment_chat -> {
-                    appbar.liftOnScrollTargetViewId = R.id.recycler_view_chat
+                    setAppBarLiftOnScroll(R.id.recycler_view_chat)
                 }
 
                 R.id.fragment_chats -> {
-                    appbar.liftOnScrollTargetViewId = R.id.recycler_view_chats
+                    setAppBarLiftOnScroll(R.id.recycler_view_chats)
                 }
 
                 R.id.fragment_tools -> {
-                    appbar.liftOnScrollTargetViewId = R.id.recycler_view_tools
+                    setAppBarLiftOnScroll(R.id.recycler_view_tools)
+                }
+
+                R.id.fragment_settings -> {
+                    setAppBarLiftOnScroll(androidx.preference.R.id.recycler_view)
                 }
 
                 R.id.fragment_about -> {
-                    appbar.liftOnScrollTargetViewId = R.id.about_scroll_view
+                    setAppBarLiftOnScroll(R.id.about_scroll_view)
                 }
 
                 R.id.fragment_select_text -> {
-                    appbar.liftOnScrollTargetViewId = R.id.select_text_scroll_view
+                    setAppBarLiftOnScroll(R.id.select_text_scroll_view)
                 }
 
                 R.id.fragment_voice_assistant -> {
@@ -38,7 +39,7 @@ class MainActivity : DrawerNavigationActivity() {
                 }
 
                 else -> {
-                    appbar.liftOnScrollTargetViewId = android.R.id.content
+                    setAppBarLiftOnScroll(android.R.id.content)
                 }
             }
         }
